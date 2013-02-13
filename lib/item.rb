a86da7fb9b3846abb530030fedd77cd91b798621
lalalainexd <lalalainexd@gmail.com>
2013-02-13 16:43:38 -0700

    added tests for and implentations of finding all items by name, unit price, merchant id

--- lib/item.rb
+++ lib/item.rb
@@ -24,23 +24,35 @@ attr_accessor :id, :name, :description, :unit_price, :merchant_id, :created_at, 
     @@items.sample
   end
 
-	def self.find_by_id id
-		@@items.find{|item| item.id == id}
-	end
+  def self.find_by_id id
+    @@items.find{|item| item.id == id}
+  end
 
-	def self.find_by_name name
-		@@items.find{|item| item.name == name}
-	end
+  def self.find_by_name name
+    @@items.find{|item| item.name == name}
+  end
 
-	def self.find_by_description description
-		@@items.find{|item| item.description == description}
-	end
+  def self.find_by_description description
+    @@items.find{|item| item.description == description}
+  end
 
-	def self.find_by_unit_price unit_price
-		@@items.find{|item| item.unit_price == unit_price}
-	end
+  def self.find_by_unit_price unit_price
+    @@items.find{|item| item.unit_price == unit_price}
+  end
 
-	def self.find_by_merchant_id merchant_id
-		@@items.find{|item| item.merchant_id == merchant_id}
-	end
+  def self.find_by_merchant_id merchant_id
+    @@items.find{|item| item.merchant_id == merchant_id}
+  end
+
+  def self.find_all_by_name name
+    @@items.find_all{|item|item.name == name}
+  end
+
+  def self.find_all_by_merchant_id merchant_id
+    @@items.find_all{|item|item.merchant_id == merchant_id}
+  end
+
+  def self.find_all_by_unit_price unit_price
+    @@items.find_all{|item|item.unit_price == unit_price}
+  end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -44,73 +44,136 @@ class ItemTest < MiniTest::Unit::TestCase
    end
 
   def test_it_returns_a_random_item
-		CsvLoader.load_items
+    CsvLoader.load_items
 
     item1 = Item.random
     item2 = Item.random
-		refute_equal item1, item2
+    refute_equal item1, item2
 
   end
 
-	def test_it_finds_an_item_by_id
-		CsvLoader.load_items
+  def test_it_finds_an_item_by_id
+    CsvLoader.load_items
 
-		id = "1"
-		item = Item.find_by_id id
-		assert_equal id, item.id
+    id = "1"
+    item = Item.find_by_id id
+    assert_equal id, item.id
 
-		id = "2"
-		item = Item.find_by_id id
-		assert_equal id, item.id
-	end
+    id = "2"
+    item = Item.find_by_id id
+    assert_equal id, item.id
+  end
 
-	def test_it_finds_an_item_by_name
-		CsvLoader.load_items
+  def test_it_finds_an_item_by_name
+    CsvLoader.load_items
 
-		name = "Item Qui Esse"
-		item = Item.find_by_name name
-		assert_equal name, item.name
+    name = "Item Qui Esse"
+    item = Item.find_by_name name
+    assert_equal name, item.name
 
-		name = "Item Autem Minima"
-		item = Item.find_by_name name
-		assert_equal name, item.name
-	end
+    name = "Item Autem Minima"
+    item = Item.find_by_name name
+    assert_equal name, item.name
+  end
 
-	def test_it_finds_an_item_by_description
-		CsvLoader.load_items
+  def test_it_finds_an_item_by_description
+    CsvLoader.load_items
 
-		description = "Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro."
-		item = Item.find_by_description description
-		assert_equal description, item.description
+    description = "Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro."
+    item = Item.find_by_description description
+    assert_equal description, item.description
 
-		description = "Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora."
-		item = Item.find_by_description description
-		assert_equal description, item.description
-	end
+    description = "Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora."
+    item = Item.find_by_description description
+    assert_equal description, item.description
+  end
 
 
-	def test_it_finds_an_item_by_unit_price
-		CsvLoader.load_items
+  def test_it_finds_an_item_by_unit_price
+    CsvLoader.load_items
 
-		unit_price = "75107"
-		item = Item.find_by_unit_price unit_price
-		assert_equal unit_price, item.unit_price
+    unit_price = "75107"
+    item = Item.find_by_unit_price unit_price
+    assert_equal unit_price, item.unit_price
 
-		unit_price = "67076"
-		item = Item.find_by_unit_price unit_price
-		assert_equal unit_price, item.unit_price
-	end
+    unit_price = "67076"
+    item = Item.find_by_unit_price unit_price
+    assert_equal unit_price, item.unit_price
+  end
 
-	def test_it_finds_an_item_by_merchant_id
-		CsvLoader.load_items
+  def test_it_finds_an_item_by_merchant_id
+    CsvLoader.load_items
 
-		merchant_id = "1"
-		item = Item.find_by_merchant_id merchant_id
-		assert_equal merchant_id, item.merchant_id
+    merchant_id = "1"
+    item = Item.find_by_merchant_id merchant_id
+    assert_equal merchant_id, item.merchant_id
 
-		merchant_id = "2"
-		item = Item.find_by_merchant_id merchant_id
-		assert_equal merchant_id, item.merchant_id
-	end
+    merchant_id = "2"
+    item = Item.find_by_merchant_id merchant_id
+    assert_equal merchant_id, item.merchant_id
+  end
 
+  def test_it_find_all_items_by_name
+    CsvLoader.load_items
+
+    name = "Item Qui Esse"
+    items = Item.find_all_by_name name
+    assert_equal 1, items.size
+
+    name = "Item Autem Minima"
+    items = Item.find_all_by_name name
+    assert_equal 1, items.size
+
+  end
+
+  def test_it_returns_empty_for_nonexistant_name
+    CsvLoader.load_items
+
+    name = "blah"
+    items = Item.find_all_by_name name
+    assert items.empty?
+
+  end
+
+  def test_it_find_all_items_by_merchant_id
+    CsvLoader.load_items
+
+    merchant_id = "1"
+    items = Item.find_all_by_merchant_id merchant_id
+    assert_equal 15, items.size
+
+    merchant_id = "2"
+    items = Item.find_all_by_merchant_id merchant_id
+    assert_equal 38, items.size
+
+  end
+
+  def test_it_returns_empty_for_nonexistant_merchant_id
+    CsvLoader.load_items
+
+    merchant_id = "blah"
+    items = Item.find_all_by_merchant_id merchant_id
+    assert items.empty?
+  end
+
+  def test_it_find_all_items_by_unit_price
+    CsvLoader.load_items
+
+    unit_price = "99028"
+    items = Item.find_all_by_unit_price unit_price
+    assert_equal 2, items.size
+
+    unit_price = "98005"
+    items = Item.find_all_by_unit_price unit_price
+    assert_equal 1, items.size
+
+  end
+
+  def test_it_returns_empty_for_nonexistant_unit_price
+    CsvLoader.load_items
+
+    unit_price = "23589012301"
+    items = Item.find_all_by_unit_price unit_price
+    assert items.empty?
+  end
 end
