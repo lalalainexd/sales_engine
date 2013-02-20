e664c8488c8819d8ebc7bbf5b57d2ef061fde807
lalalainexd <lalalainexd@gmail.com>
2013-02-20 11:19:03 -0700

    added tests and implementation for random, find_by_credit_card_number, and find_all_by_status

--- lib/sales_engine/transaction_finder.rb
+++ lib/sales_engine/transaction_finder.rb
@@ -1,12 +1,24 @@
 module SalesEngine
   module TransactionFinder
 
+    def random
+      transactions.sample
+    end
+
     def find_by_id id
       transactions.find {|transaction| transaction.id == id}
     end
 
     def find_all_by_invoice_id(id)
       transactions.find_all {|transaction| transaction.invoice_id == id}
+    end
+
+    def find_by_credit_card_number card_number
+      transactions.find{|t| t.credit_card_number == card_number}
+    end
+
+    def find_all_by_result result
+      transactions.find_all{|t| t.result == result}
     end
 
   end
