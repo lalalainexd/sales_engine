f2c225ce3997a1afb4ced88a566dce77d24cec71
Laura Steadman <lauramsteadman@gmail.com>
2013-02-20 11:34:13 -0700

    added method for invoice class

--- lib/sales_engine/invoice_finder.rb
+++ lib/sales_engine/invoice_finder.rb
@@ -37,6 +37,10 @@ module SalesEngine
       invoices.find_all{|invoice| invoice.created_at == date}
     end
 
+    def find_all_by_status status
+      invoices.find_all {|invoice| invoice.status == status}
+    end
+
     def find_all_by_updated_at date
       invoices.find_all{|invoice| invoice.updated_at == date}
     end
