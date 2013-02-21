63fb31a53403aaad5e12df2a3454565fdc9d4657
Laura Steadman <lauramsteadman@gmail.com>
2013-02-20 17:36:30 -0700

    Re-ordered

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
-require 'minitest/autorun'
-require 'minitest/pride'
 require 'simplecov'
 
 SimpleCov.start do
   add_filter "test"
 end
 
+require 'minitest/autorun'
+require 'minitest/pride'
 
 require 'sales_engine'
 
